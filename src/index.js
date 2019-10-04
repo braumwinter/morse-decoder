3eeb89eeae362a46f57559f6ba3f5cbcd6f353de
Kate <braumwinter@gmail.com>
2019-10-04 15:59:29 +0300

    solved the task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result_str = "";
+
+    for (let iter_expr = 0; iter_expr < expr.length; iter_expr = iter_expr + 10) {
+        let temp_str = expr.slice(iter_expr, iter_expr + 10);
+        let morse_str = "";
+
+        if (temp_str === "**********") {
+            result_str += " ";
+
+        } else {
+            for (let iter_temp_str = 0; iter_temp_str < temp_str.length; iter_temp_str = iter_temp_str + 2) {
+                let temp_substr = temp_str.slice(iter_temp_str, iter_temp_str + 2);
+
+                if (temp_substr === "10") {
+                    morse_str += ".";
+                } else if (temp_substr === "11") {
+                    morse_str += "-";
+                }
+            }
+
+            for (let key in MORSE_TABLE) {
+                if( key === morse_str){
+                    result_str += MORSE_TABLE[key];
+                }
+            }
+        }
+    }
+
+    return result_str;
 }
 
 module.exports = {
     decode
-}
+};
